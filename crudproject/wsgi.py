993e101353e90745b315377976743499fc8a2eb5
jeongyeonchoi <49220116+jeongyeonchoi@users.noreply.github.com>
2019-07-26 23:04:19 +0900

    11
    11

--- crudproject/wsgi.py
+++ crudproject/wsgi.py
@@ -11,10 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise import DjangoWhiteNoise
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crudproject.settings')
 
 application = get_wsgi_application()
 
-application = DjangoWhiteNoise(application)
